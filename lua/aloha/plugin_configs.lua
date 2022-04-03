8a5c2cbc60aed04542f790f84e2aad0637352f38
alohaia <liu.qihuan@outlook.com>
2022-04-03 20:49:35 +0800

    fix: config for olimorris/onedarkpro.nvim

--- lua/aloha/plugin_configs.lua
+++ lua/aloha/plugin_configs.lua
@@ -169,7 +169,7 @@ configs['kyazdani42/nvim-tree.lua'] = function()
           global = false,
         },
         open_file = {
-          quit_on_open = false,
+          quit_on_open = true,
           resize_window = false,
           window_picker = {
             enable = true,
@@ -806,39 +806,18 @@ end
 configs['olimorris/onedarkpro.nvim'] = function()
     vim.opt.background = "dark"
     require("onedarkpro").setup({
-        -- Theme can be overwritten with 'onedark' or 'onelight' as a string
-        theme = function()
-            if vim.o.background == "dark" then
-                return "onedark"
-            else
-                return "onelight"
-            end
-        end,
-        colors = {}, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
-        hlgroups = {}, -- Override default highlight groups
-        filetype_hlgroups = {}, -- Override default highlight groups for specific filetypes
-        plugins = { -- Override which plugins highlight groups are loaded
-            native_lsp = true,
-            polygot = true,
-            treesitter = true,
-            -- NOTE: Other plugins have been omitted for brevity
+        hlgroups = {
+            FoldColumn = { link = "Normal" }
         },
-        styles = {
-            strings = "NONE", -- Style that is applied to strings
-            comments = "NONE", -- Style that is applied to comments
-            keywords = "NONE", -- Style that is applied to keywords
-            functions = "NONE", -- Style that is applied to functions
-            variables = "NONE", -- Style that is applied to variables
+        plugins = {
+            all = false,
+            native_lsp = true,
+            polygot = false,
+            treesitter = true
         },
         options = {
-            bold = false, -- Use the themes opinionated bold styles?
-            italic = false, -- Use the themes opinionated italic styles?
-            underline = false, -- Use the themes opinionated underline styles?
-            undercurl = true, -- Use the themes opinionated undercurl styles?
-            cursorline = true, -- Use cursorline highlighting?
-            transparency = true, -- Use a transparent background?
-            terminal_colors = false, -- Use the theme's colors for Neovim's :terminal?
-            window_unfocussed_color = false, -- When the window is out of focus, change the normal background?
+            transparency = true,
+            window_unfocussed_color = false,
         }
     })
     require("onedarkpro").load()
