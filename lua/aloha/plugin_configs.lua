483423e93cece2106cc004c60d1baac60474ef42
alohaia <liu.qihuan@outlook.com>
2022-10-19 20:43:34 +0800

    fix: cmp_nvim_lsp.update_capabilities is deprecated

--- lua/aloha/plugin_configs.lua
+++ lua/aloha/plugin_configs.lua
@@ -272,7 +272,7 @@ configs['neovim/nvim-lspconfig'] = function()
 
     -- local capabilities = vim.lsp.protocol.make_client_capabilities()
     -- capabilities.textDocument.completion.completionItem.snippetSupport = true
-    local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
     local runtime_path = vim.split(package.path, ';')
     table.insert(runtime_path, "lua/?.lua")
